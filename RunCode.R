61807edf41c36a07cc3991368232bde61ce8a575
cassioaumonti <cassioaumonti@yahoo.com>
2022-10-10 16:13:37 -0400

    test2

--- RunCode.R
+++ RunCode.R
@@ -7,6 +7,14 @@ rmarkdown::render(input = "Monti_Patnaik_project2T_ST558.Rmd",
                     or_something = TRUE,
                     toc = TRUE,
                     df_print = "tibble"
-                  )
+                  ),
+                  runtime = "static",
+                  clean = TRUE,
+                  params = NULL,
+                  knit_meta = NULL,
+                  envir = parent.frame(),
+                  run_pandoc = TRUE,
+                  quiet = FALSE,
+                  encoding = "UTF-8"
 )
 
