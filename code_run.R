ac03e65d490f2963496409f952dc1687e6e10878
cassioaumonti <cassioaumonti@yahoo.com>
2022-10-10 10:20:02 -0400

    add run code .R

---
+++ code_run.R
@@ -0,0 +1,8 @@
+
+rmarkdown::render("C:/NCSU/Classes/2022-2/ST 558/Projects/P2/project2T/README.Rmd",
+                  output_format = "github_document",output_options = list(
+                    name_value_pairs = "value",
+                    or_something = TRUE,
+                    df_print = "tibble"
+                  )
+)
