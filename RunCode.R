13d0254f73ac24af4f3ffecef2af4d9773ad760b
cassioaumonti <cassioaumonti@yahoo.com>
2022-10-10 15:48:27 -0400

    commit3

--- RunCode.R
+++ RunCode.R
@@ -1,5 +1,6 @@
 
-rmarkdown::render(input = "README.Rmd",
+rmarkdown::render(input = "Monti_Patnaik_project2T_ST558.Rmd",
+                  output_file = "README.md",
                   output_format = "github_document",
                   output_options = list(
                     name_value_pairs = "value",
